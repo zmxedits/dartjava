f56529054650dc784c20ef07e8b61fd6fd65fd69
zmxedits <106353803+zmxedits@users.noreply.github.com>
2022-10-20 10:13:00 +0200

    dart.java

---
+++ dart.java
@@ -0,0 +1,41 @@
+public void fuehreAus()
+{
+  meinStift.bewegeBis(700, 100);
+  meinStift.zeichneKreis(10);
+  meinStift.zeichneKreis(20);
+  meinStift.zeichneKreis(30);
+  meinStift.zeichneKreis(40);
+  
+  int x = 300;
+  int c = 350;
+  int y = 230;
+  int v = 200; 
+  int b = 0;
+  
+  while(b < 800){
+    b++;
+  }
+  
+  while(x < 700){
+     meinStift.normal();
+     meinStift.hoch();
+     meinStift.bewegeBis(x, y);
+     meinStift.runter();
+     meinStift.bewegeBis(c, v);
+  }
+  
+  try{
+    Thread.sleep(20);
+  } catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+  }
+
+  meinStift.radiere();
+  meinStift.bewegeBis(x, y);
+  meinStift.runter();
+  meinStift.bewegeBis(c, v);
+  meinStift.zeichneKreis(2);
+  
+  x++;
+  c++;
+}
